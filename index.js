ce9f72b2b6802f3f890b83b1b2f43df0cfb0738a
stacy-morgan <ben@Fejes-MacBook-Air.local>
2024-06-30 16:03:13 -0700

    index.html usable + js & css

---
+++ index.js
@@ -0,0 +1,5 @@
+const gotoPhotography = document.getElementById('goto-photography-site')
+
+gotoPhotography.addEventListener('click', function() {
+    window.location.href = 'stacy-morgan.github.io/html/homepage.html';
+});
