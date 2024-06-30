81fcde89cf0d6b1fffc1e0eb22de0b45a1ec960b
stacy-morgan <ben@Fejes-MacBook-Air.local>
2024-06-30 15:21:02 -0700

    Push test from CLI

---
+++ js/gallery.js
@@ -0,0 +1,26 @@
+const jp_ny = document.getElementById('jp_ny');
+const jp_t = document.getElementById('jp_t');
+const homeBtn = document.getElementById('homeBtn')
+const galleryBtn = document.getElementById('galleryBtn')
+const aboutBtn = document.getElementById('aboutBtn')
+const contactBtn = document.getElementById('contactBtn')
+
+homeBtn.addEventListener('click', function() {
+    window.location.href = 'homepage.html';
+});
+
+galleryBtn.addEventListener('click', function() {
+    window.location.href = 'gallery.html';
+});
+
+aboutBtn.addEventListener('click', function() {
+    window.location.href = 'about.html';
+});
+
+jp_ny.addEventListener('click', function() {
+    window.location.href = 'gallery/ny_c.html'
+});
+
+jp_t.addEventListener('click', function() {
+    window.location.href = '../html/gallery/ny_c.html';
+});
---
+++ js/galleryWebpage.js
@@ -0,0 +1,28 @@
+//main.js
+const homeBtn = document.getElementById('homeBtn');
+const galleryBtn = document.getElementById('galleryBtn');
+const aboutBtn = document.getElementById('aboutBtn');
+const contactBtn = document.getElementById('contactBtn');
+const EMT = document.getElementById('4_---');
+
+const click1 = document.getElementById('click1');
+
+homeBtn.addEventListener('click', function() {
+    window.location.href = '../homepage.html';
+});
+
+galleryBtn.addEventListener('click', function() {
+    window.location.href = '../gallery.html';
+});
+
+aboutBtn.addEventListener('click', function() {
+    window.location.href = '../about.html';
+});
+
+contactBtn.addEventListener('click', function() {
+    window.location.href = '../contact.html';
+});
+
+EMT.addEventListener('click', function() {
+    alert("EMT");
+});
