63c1edbc90e65e38be9bca08d29a1fd6d7fff80c
stacy-morgan <stacy.morgan.fejes@gmail.com>
2024-07-18 23:14:04 -0700

    Updated readme with tasks for the future

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ### -Fix image filenames in caption
 ### ~~-Remove file size display~~
 ### -Add ISO to picture data displays
+#
+#
+### Check img paths for all San Diego subdests
+### Add captions for all San Diego subdests
+#
--- js/destinationJS/sd/5ftci_galleryLoader.js
+++ js/destinationJS/sd/5ftci_galleryLoader.js
@@ -8,18 +8,6 @@ const b_i5btn = document.getElementById("b_i5");
 const b_i6btn = document.getElementById("b_i6");
 const b_i7btn = document.getElementById("b_i7");
 const b_i8btn = document.getElementById("b_i8");
-const b_i9btn = document.getElementById("b_i9");
-const b_i10btn = document.getElementById("b_i10");
-const b_i11btn = document.getElementById("b_i11");
-const b_i12btn = document.getElementById("b_i12");
-const b_i13btn = document.getElementById("b_i13");
-const b_i14btn = document.getElementById("b_i14");
-const b_i15btn = document.getElementById("b_i15");
-const b_i16btn = document.getElementById("b_i16");
-const b_i17btn = document.getElementById("b_i17");
-const b_i18btn = document.getElementById("b_i18");
-const b_i19btn = document.getElementById("b_i19");
-const b_i20btn = document.getElementById("b_i20");
 
 const picture_title_disp = document.getElementById("picture-title")
 const picture_data_disp = document.getElementById("picture-data")
@@ -32,22 +20,10 @@ const picture_titles = [
     "Festival",
     "Night Markets",
     "Gazebo",
-    "Miniature Starbucks!",
-    "Possible Clock Tower",
-    "Modern Gathering Area",
-    "Highway Traffic",
-    "Parked Car",
-    "Toronto Sign",
-    "Highway & Tow Truck",
-    "Skyline",
-    "Cut Off Highway",
-    "Bridge and Sky",
-    "Big Bridge & Gathering",
-    "Empty Turnpike",
-    "Carnival & Hot Air Balloons"
+    "Miniature Starbucks!"
 ]
 
-const maxImages = 20;
+const maxImages = 8;
 let imgNum = 0;
 let index = 1;
 
@@ -106,66 +82,6 @@ b_i7btn.addEventListener('click', function() {
 
 b_i8btn.addEventListener('click', function() {
     imgNum = 7;
-    refreshImg();
-});
-
-b_i9btn.addEventListener('click', function() {
-    imgNum = 8;
-    refreshImg();
-});
-
-b_i10btn.addEventListener('click', function() {
-    imgNum = 9;
-    refreshImg();
-});
-
-b_i11btn.addEventListener('click', function() {
-    imgNum = 10;
-    refreshImg();
-});
-
-b_i12btn.addEventListener('click', function() {
-    imgNum = 11;
-    refreshImg();
-});
-
-b_i13btn.addEventListener('click', function() {
-    imgNum = 12;
-    refreshImg();
-});
-
-b_i14btn.addEventListener('click', function() {
-    imgNum = 13;
-    refreshImg();
-});
-
-b_i15btn.addEventListener('click', function() {
-    imgNum = 14;
-    refreshImg();
-});
-
-b_i16btn.addEventListener('click', function() {
-    imgNum = 15;
-    refreshImg();
-});
-
-b_i17btn.addEventListener('click', function() {
-    imgNum = 16;
-    refreshImg();
-});
-
-b_i18btn.addEventListener('click', function() {
-    imgNum = 17;
-    refreshImg();
-});
-
-b_i19btn.addEventListener('click', function() {
-    imgNum = 18;
-    refreshImg();
-});
-
-b_i20btn.addEventListener('click', function() {
-    imgNum = 19;
     refreshImg();
 });
 
